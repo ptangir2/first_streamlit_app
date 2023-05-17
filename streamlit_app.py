842456fea8c44c9db07883bddc7385ec53c3498c
ptangir2 <133858788+ptangir2@users.noreply.github.com>
2023-05-17 15:51:17 +0530

    Update streamlit_app.py
    corrected duplicte widget by adding key

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,6 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado',
 streamlit.dataframe(my_fruit_list)
 
 # Filter the Table Data
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),key='Strawberries')
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
